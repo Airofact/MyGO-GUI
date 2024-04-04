888b8cb75321673ddc6e42519afc17dd12aad1c2
Airo <bigod857861926@163.com>
2024-04-04 22:10:27 +0800

    abstract out TransactionComponent

---
+++ src/components/TransactionComponent.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { Transaction } from '../libs/types'
+defineProps<Transaction>()
+</script>
+
+<template>
+	<div>
+		<ion-avatar slot="start">
+			<ion-img alt="头像" :src="`https://picsum.photos/80/80?random=${id}`" />
+		</ion-avatar>
+		<ion-label>
+			<h2>{{ title }}</h2>
+			<p>{{ description }}</p>
+		</ion-label>
+	</div>
+</template>
