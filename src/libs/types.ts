d41d87e8ae58b63623a9ceb6ea73d8113f0b2c44
Airo <bigod857861926@163.com>
2024-04-06 13:45:29 +0800

    add type TransactionStatus&Role

--- src/libs/types.ts
+++ src/libs/types.ts
@@ -7,3 +7,17 @@ export interface Transaction {
   createdAt: number
   updatedAt: number
 }
+
+export enum TransactionStatus {
+  DRAFT = "Draft",
+  CENSORING = "Censoring",
+  REJECT = "Reject",
+  PASSED = "Passed"
+}
+
+//export enum Role
+export enum Role {
+  OLD = "Old",
+  VOLUNTEER = "Volunteer",
+  ADMIN = "Adim"
+}
