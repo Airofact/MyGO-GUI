df9471b83e71a29a0346479e0a33acec95db3f89
Airo <bigod857861926@163.com>
2024-04-06 19:57:32 +0800

    add TransactionComponent&misc fix

--- src/components/TransactionComponent.vue
+++ src/components/TransactionComponent.vue
@@ -1,16 +1,14 @@
 <script setup lang="ts">
-import { Transaction } from '../libs/types'
+import { Transaction } from '@/libs/types'
 defineProps<{data:Transaction}>()
 </script>
 
 <template>
-	<div>
-		<ion-avatar slot="start">
-			<ion-img alt="头像" :src="`https://picsum.photos/80/80?random=${data.id}`" />
-		</ion-avatar>
-		<ion-label>
-			<h2>{{ data.title }}</h2>
-			<p>{{ data.description }}</p>
-		</ion-label>
-	</div>
+	<ion-avatar slot="start">
+		<ion-img alt="头像" :src="`https://picsum.photos/80/80?random=${data.id}`" />
+	</ion-avatar>
+	<ion-label>
+		<h2>{{ data.title }}</h2>
+		<p>{{ data.description }}</p>
+	</ion-label>
 </template>
---
+++ src/components/TransactionDetailComponent.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { Transaction } from '@/libs/types'
+defineProps<{data:Transaction}>()
+</script>
+
+<template>
+	<ion-card>
+		<ion-card-header>
+			<ion-card-title>
+				{{ data.title }}
+			</ion-card-title>
+			<ion-card-subtitle>
+				该需求由{{ data.user_id }}于{{ data.createdAt }}创建 更新于{{ data.updatedAt }}
+			</ion-card-subtitle>
+		</ion-card-header>
+		<ion-card-content>
+			{{ data.description }}
+		</ion-card-content>
+	</ion-card>
+</template>
